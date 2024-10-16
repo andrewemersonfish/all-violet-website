7d399a9d9d335310d1051a0aeb054d919db4df4f
andrewemersonfish <andrew@fishandrew.com>
2024-10-16 19:18:59 -0400

    changing pasture

--- components/RetroHero.tsx
+++ components/RetroHero.tsx
@@ -4,18 +4,18 @@ import Link from 'next/link'
 export default function RetroHero() {
   return (
     <div style={{ textAlign: 'center', padding: '20px', backgroundColor: 'white' }}>
-      <h1 style={{ fontSize: '48px', color: '#FF00FF', fontFamily: 'Comic Sans MS, cursive' }}>
-        Welcome to All Violet's Website!
+      <h1 style={{ fontSize: '48px', color: '#FF00FF', fontFamily: 'Courier New, bold' }}>
+        All Violet The Band
       </h1>
       <div className="marquee">
         <p style={{ fontSize: '24px', color: '#FFFF00' }}>
-          <strong>New album "Digital Dreams" dropping on 07.07.2003!</strong>
+          <strong style={{ color: '#00FFFF' }}>NEON SEEPING FROM THE PAVEMENT</strong>
         </p>
       </div>
-      <Image src="/Houndstooth.jpg" width={300} height={200} alt="Houndstooth Pattern" />
-      <div>
-        <Image src="/images/divider.gif" width={500} height={20} alt="Divider" />
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Image src="/changing_pasture.jpg" width={500} height={20} alt="Divider" />
       </div>
+      
     </div>
   )
 }
